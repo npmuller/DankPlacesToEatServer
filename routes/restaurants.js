3c9f85f906c863e79a33b843c6c90087bcad8892
Nathan Muller <npmuller67@gmail.com>
2018-02-20 17:44:35 -0500

    added code to get all individual restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,7 +1,39 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var router = express.Router();
-var _ = require('lodash');
+var collections = require('../models/collections.js');
 var models = require('../models/models.js');
-var knex = require('../database/database.js').knex;
 
+// Get all restaurants
+router.route('/').get(function (req, res) {
+    console.info('Got request for all restaurants!');
+    collections.restaurants.forge()
+    .fetch()
+    .then(function (collection) {
+        res.json({restaurants: collection.toJSON()});
+    })
+    .catch(function (err) {
+        console.error('Error getting restaurants! ' + err.message);
+        res.status(500).json({message: err.message});
+    });
+});
+
+// Get a particular restaurant
+router.route('/:id').get(function(req, res) {
+    var restaurantId = req.params.id;
+    console.info('got request for restaurant id ' + restaurantId);
+    models.restaurant.forge({id: restaurantId})
+    .fetch()
+    .then(function (restaurant) {
+        if (!restaurant) {
+            console.error('Error getting restaurant ' + restaurantId);
+            res.status(404).json({error: true, data: {}});
+        }
+        else {
+            res.json(restaurant.toJSON());
+        }
+    })
+    .catch(function (err) {
+        console.error('Error getting restaurant ' + restaurantId + ', ' + err.message);
+        res.status(500).json({message: err.message});
+    });
+});
