d65bc5ec97ecc3f2dc46612c51d7514fc2110b44
Nathan Muller <npmuller67@gmail.com>
2018-02-24 11:37:50 -0500

    added drop procedure statement before creating it to ensure procedure gets updated.

--- database/stored_procedures/sp_get_restaurants_by_distance.sql
+++ database/stored_procedures/sp_get_restaurants_by_distance.sql
@@ -10,7 +10,9 @@
  * ============= pCenterLong double
  * ============= pRadius int (expressed in miles)
  */
-delimeter $$
+use dank;
+drop procedure if exists sp_get_restaurants_by_distance;
+delimiter $$
 create procedure sp_get_restaurants_by_distance(
     in pCenterLat double,
     in pCenterLong double,
@@ -18,40 +20,38 @@ create procedure sp_get_restaurants_by_distance(
 )
  
 begin
-    select concat("[", 
-    (
-        select group_concat(select concat("{",
-            (
-                select
-                    concat("id: ", r.id, ","),
-                    -- DEF NEED THESE ITEMS...DELIVER THE REST LATER WHEN YOU CLICK ON A SPECIFIC RESTAURANT
-                    concat("n: \"", r.name, "\","),
-                    concat("la: ", r.latitude, ","),
-                    concat("lo: ", r.longitude)
-                from
-                    restaurant r
-                where
-                    -- From https://stackoverflow.com/questions/8994718/mysql-longitude-and-latitude-query-for-other-rows-within-x-mile-radius
-                    select (
-                        6371 *
-                        acos(
-                            cos(radians(pCenterLat)) *
-                            cos(radians(r.latitude)) *
-                            cos(
-                                radians(r.longitude) - radians(pCenterLong)
-                            ) +
-                            sin(radians(pCenterLat)) *
-                            sin(radians(r.latitude))
-                        )
-                    ) as distance < pRaidus
-                order by r.id
-                limit 20
+    select
+        concat("[", 
+            group_concat(
+                concat("{id: ", r.id, ","),
+                -- DEF NEED THESE ITEMS...DELIVER THE REST LATER WHEN YOU CLICK ON A SPECIFIC RESTAURANT
+                concat("n: \"", r.name, "\","),
+                concat("la: ", r.latitude, ","),
+                concat("lo: ", r.longitude, "}")
             ),
-            "}"
+            "]"
+        ) as json
+    from
+        restaurant r
+    where
+        -- Modified from https://stackoverflow.com/questions/8994718/mysql-longitude-and-latitude-query-for-other-rows-within-x-mile-radius
+        pRadius > (select
+            6371 *
+            acos(
+                cos(radians(pCenterLat)) *
+                cos(radians(r.latitude)) *
+                cos(
+                    radians(r.longitude) - radians(pCenterLong)
+                ) +
+                sin(radians(pCenterLat)) *
+                sin(radians(r.latitude))
+            )
         )
-    ),
-    "]"
-    ) as json;
-end
+    order by
+        r.id
+    limit
+        20
+    ;
+end$$
 
-delimeter ;
+delimiter ;
